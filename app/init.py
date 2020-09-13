fce3f061d8ddd59ee38a873cc7a623d4d994b7a9
Nicole O'Connor <lavacano@lavacano.net>
2020-09-13 14:48:44 -0700

    i like video games

--- app/init.py
+++ app/init.py
@@ -8,10 +8,14 @@ app = Flask(__name__)
 def favicon():
     return redirect("https://cdn.otl-hga.net/index/favicon.png", code=301)
 
+
 @app.route('/', defaults={"path": ""})
 @app.route("/<path:path>") 
 def index_page(path):
-    return render_template("index")
+    context = {"title": "otl-hga.net", "styles": [], "scripts": []}
+    context["styles"].append("https://cdn.otl-hga.net/index/css/index.css")
+    context["scripts"].append("https://cdn.otl-hga.net/gradius/gradius.js")
+    return render_template("index_new", **context)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
