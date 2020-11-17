e15ad294f6b668f3291b603732617ba27948cf5b
Nicole O'Connor <lavacano@lavacano.net>
2020-11-16 16:37:06 -0800

    basic clacks overhead

--- app/init.py
+++ app/init.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import random
+
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
@@ -16,6 +18,18 @@ def index_page(path):
     context["styles"].append("https://cdn.otl-hga.net/index/css/index.css")
     context["scripts"].append("https://cdn.otl-hga.net/gradius/gradius.js")
     return render_template("index_new", **context)
+
+@app.after_request
+def clacks_overhead(resp):
+    names_to_honor = [
+            "Breonna Taylor",
+            "George Floyd",
+            "Trayvon Martin",
+            "Terry Pratchett"
+        ]
+
+    resp.headers["X-Clacks-Overhead"] = "GNU {0}".format(random.choice(names_to_honor))
+    return resp
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
